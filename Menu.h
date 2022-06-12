fd2451031f29b39e6ce13afb2fb14d909796915f
ApteryxXYZ <apteryx.xyz@gmail.com>
2022-06-12 23:27:13 +1200

    Update Menu class to use new Util methods

--- Menu.h
+++ Menu.h
@@ -3,12 +3,7 @@
 #include <string>
 #include <vector>
 
-// Clear screen cross platform
-#ifdef _WIN32
-#define CLEAR "cls"
-#else
-#define CLEAR "clear"
-#endif
+#include "Util.h"
 
 using namespace std;
 
@@ -18,43 +13,10 @@ public:
     string header;
     vector<string> options;
 
-    // Ask for a number from the user
-    int requestNumber(int min, int max) {
-        int num; string str;
-        cout << "> ";
-        cin >> str;
-
-        try {
-            // Try and convert the inputted string to an integer
-            num = stoi(str);
-        }
-        catch (exception e) {
-            // If it cant be converted, it is not a number
-            cout << "Input is not a number, please try again.\n";
-            return requestNumber(min, max);
-        }
-
-        // Ensure the number is within the range
-        if (num < min || num > max) {
-            cout << "Input is out of range, please try again.\n";
-            return requestNumber(min, max);
-        }
-
-        return num;
-    }
-
-    // Ask for a string from the user
-    string requestString() {
-        string str;
-        cout << "> ";
-        cin >> str;
-        return str;
-    }
-    
     // Call this menu
     void run(bool clearBefore, bool clearAfter) {
         // Clear the screen if requested
-        if (clearBefore) system(CLEAR);
+        if (clearBefore) Util::clearScreen();
         cout << header;
 
         // Print the options
@@ -63,10 +25,10 @@ public:
         
         // Request a selection from the user
         int maxOption = options.size();
-        int selectedOption = requestNumber(1, maxOption);
+        int selectedOption = Util::requestInteger(1, maxOption);
         
         // Clear the screen if requested
-        if (clearAfter) system(CLEAR);
+        if (clearAfter) Util::clearScreen();
 
         // Call the handleOption method with the selected option
         bool runAgain = handleOption(selectedOption);
@@ -82,15 +44,12 @@ public:
         return false;
     }
 
-	// Quick way to show a basic mini menu
+    // Quick way to show a basic mini menu
     static int runMiniMenu(vector<string> options) {
-        // Create a menu object so we can access the requestNumber method
-        Menu menu;
-
         for (int i = 0; i < options.size(); i++)
             cout << i + 1 << ". " << options[i] << endl;
 
-		int maxOption = options.size();
-		return menu.requestNumber(1, maxOption);
+        int maxOption = options.size();
+        return Util::requestInteger(1, maxOption);
     }
 };
